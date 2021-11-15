5b8a9f769ee38f938ef3f123a53b6f2241244f04
Nacksss <nicolas.detienne@edu.esiee.fr>
2021-11-15 11:46:54 +0100

    début listView dans NOm

--- app/src/main/java/com/example/pyramide/ui/home/Nom.java
+++ app/src/main/java/com/example/pyramide/ui/home/Nom.java
@@ -14,25 +14,25 @@ import android.view.ViewGroup;
 
 import com.example.pyramide.R;
 
-public class Nom extends Fragment {
+public class Nom  extends NombreJoueurs {
 
-    private NomViewModel mViewModel;
-
-    public static Nom newInstance() {
+    //private NomViewModel mViewModel;
+    /*public static Nom newInstance() {
         return new Nom();
-    }
+    }*/
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.nom_fragment, container, false);
     }
-
+    /*
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(NomViewModel.class);
         // TODO: Use the ViewModel
-    }
+    }*/
 
+    
 }
